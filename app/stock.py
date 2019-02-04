c883683703de493f51f08dd6d25acb5a3da09899
InfernapeXavier <itsgood37@ymail.com>
2019-02-04 17:06:32 +0530

    Structure Change

--- app/stock.py
+++ app/stock.py
@@ -1,33 +1,20 @@
-from flask import Flask
+from app import app
 from flask import render_template
-# from test import listone
-# from toFetch import SMA, WMA, EMA
-import json
 import requests
+import json
 import datetime as dt
 from datetime import timedelta, date
 import calendar
 import holidays
 
-app=Flask(__name__)
-
-SMA = []
-WMA = []
-EMA = []
-# newstr = ''
-# for element in listone:
-# 	newstr = newstr + str(element)
-
-# @app.route("/index")
 @app.route("/")
+@app.route("/index")
 def index():
-	# return render_template('pg1.html', data=newstr)
-	# return ''.join(str(listone())).strip(' ')
 	return render_template('pg1.html')
 
 @app.route("/pg1.html")
 def pg1():
-	return render_template('pg1.html')
+	return render_template('index.html')
 
 @app.route("/company1.html")
 def company1():
@@ -38,18 +25,13 @@ def company1():
 	    APIkey = "NYZSIAK5PJ3XRW8A"
 	    r = requests.get(fetch+APIkey)
 	    data = r.json()
-	    print()
-	    print(x)
 	    for time in range(1, 15):
 	        kal = dt.date.today() - timedelta(time)
 	        if calendar.day_name[kal.weekday()] == 'Sunday' or calendar.day_name[kal.weekday()] == "Saturday" or kal in holidays.UnitedStates():
 	            continue
 	        else:
-	            print(kal.strftime("%Y-%m-%d"), end=' ')
 	            value = float(data["Technical Analysis: "+x][kal.strftime("%Y-%m-%d")][x])
 	            values.append(value)
-	            print(value)
-	print()
 	SMA = values[:9]
 	WMA = values[9:18]
 	EMA = values[18:]
@@ -57,7 +39,6 @@ def company1():
 
 @app.route("/company2.html")
 def company2():
-	# return render_template('company2.html', datedata=datelist, valuedata=valuelist)
 	return render_template('company2.html')
 
 @app.route("/company3.html")
