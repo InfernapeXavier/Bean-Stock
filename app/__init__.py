1807d00463832995da5706856670c0a6bc8d6bd4
InfernapeXavier <itsgood37@ymail.com>
2019-02-06 18:50:07 +0530

    Added Logic
    BBANDS and ADX

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+app.secret_key = "x/A?D(G+KbPeShVmYq3s6v9y$B&E)H@M"
 
 from app import stock, errors
--- app/stock.py
+++ app/stock.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session
 import requests
 import json
 import datetime as dt
@@ -11,40 +11,76 @@ import holidays
 @app.route("/index", methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
+		session['symbol'] = request.form['symbol'].upper()
 		return redirect(url_for('averages'), code=307)
 	else:
 		return render_template('index.html')
 
 @app.route("/averages", methods=['GET', 'POST'])
 def averages():
-	symbol = request.form['symbol']
-	symbol = symbol.upper()
+	symbol = session.get('symbol', None)
 	technical = ["SMA", "WMA", "EMA"]
 	values = []
 	for x in technical:
-	    fetch = "https://www.alphavantage.co/query?function="+x+"&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
-	    APIkey = "NYZSIAK5PJ3XRW8A"
-	    r = requests.get(fetch+APIkey)
-	    data = r.json()
-	    for time in range(1, 15):
-	        kal = dt.date.today() - timedelta(time)
-	        if calendar.day_name[kal.weekday()] == 'Sunday' or calendar.day_name[kal.weekday()] == "Saturday" or kal in holidays.UnitedStates():
-	            continue
-	        else:
-	            value = float(data["Technical Analysis: "+x][kal.strftime("%Y-%m-%d")][x])
-	            values.append(value)
-	SMA = values[:9]
-	WMA = values[9:18]
-	EMA = values[18:]
+		fetch = "https://www.alphavantage.co/query?function="+x+"&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
+		APIkey = "NYZSIAK5PJ3XRW8A"
+		r = requests.get(fetch+APIkey)
+		data = r.json()
+		count = 0
+		for date in data["Technical Analysis: "+x].keys():
+			count = count + 1
+			if count == 15:
+				break
+			else:
+				value = float(data["Technical Analysis: "+x][date][x])
+				values.append(value)
+	SMA = values[:14]
+	WMA = values[14:28]
+	EMA = values[28:]
 	return render_template('averages.html', sma=SMA, wma=WMA, ema=EMA)
 
 @app.route("/bband")
 def bband():
+	symbol = session.get('symbol', None)
+	technical = ["Real Upper Band", "Real Lower Band", "Real Middle Band"]
+	values = []
+	for x in technical:
+		fetch = "https://www.alphavantage.co/query?function=BBANDS&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
+		APIkey = "NYZSIAK5PJ3XRW8ASSS"
+		r = requests.get(fetch+APIkey)
+		data = r.json()
+		count = 0
+		for date in data["Technical Analysis: BBANDS"].keys():
+			count = count + 1
+			if count == 15:
+				break
+			else:
+				value = float(data["Technical Analysis: BBANDS"][date][x])
+				values.append(value)
+	UBB = values[:14]
+	MBB = values[14:28]
+	LBB = values[28:]
 	return render_template('bband.html')
 
-@app.route("/dx")
-def dx():
-	return render_template('dx.html')
+@app.route("/adx")
+def adx():
+	symbol = session.get('symbol', None)
+	values = []
+	fetch = "https://www.alphavantage.co/query?function=ADX&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
+	APIkey = "NYZSIAK5PJ3XRW8ASSS"
+	r = requests.get(fetch+APIkey)
+	data = r.json()
+	count = 0
+	for date in data["Technical Analysis: ADX"].keys():
+		count = count + 1
+		if count == 15:
+			break
+		else:
+			value = float(data["Technical Analysis: ADX"][date]["ADX"])
+			values.append(value)
+	ADX = values
+	print (ADX)
+	return render_template('adx.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
