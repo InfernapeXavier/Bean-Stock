347dca29ad6cb1fd3a274ecff94615b4aad16de5
InfernapeXavier <itsgood37@ymail.com>
2019-02-07 13:49:46 +0530

    Added Long-Term

--- app/stock.py
+++ app/stock.py
@@ -12,6 +12,7 @@ import holidays
 def index():
 	if request.method == 'POST':
 		session['symbol'] = request.form['symbol'].upper()
+		session['period'] = request.form['prediction']
 		return redirect(url_for('averages'), code=307)
 	else:
 		return render_template('index.html')
@@ -19,75 +20,95 @@ def index():
 @app.route("/averages", methods=['GET', 'POST'])
 def averages():
 	symbol = session.get('symbol', None)
+	period = session.get('period', None)
+	if period == 'short':
+		last = 15
+	else:
+		last = 31
 	technical = ["SMA", "WMA", "EMA"]
+	labels = list(range(1, last))
 	values = []
 	for x in technical:
-		fetch = "https://www.alphavantage.co/query?function="+x+"&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
+		fetch = "https://www.alphavantage.co/query?function="+x+"&symbol="+symbol+"&interval=daily&time_period="+str(last-1)+"&series_type=open&apikey="
 		APIkey = "NYZSIAK5PJ3XRW8A"
 		r = requests.get(fetch+APIkey)
 		data = r.json()
 		count = 0
 		for date in data["Technical Analysis: "+x].keys():
 			count = count + 1
-			if count == 15:
+			if count == last:
 				break
 			else:
 				value = float(data["Technical Analysis: "+x][date][x])
 				values.append(value)
-	SMA = values[:14]
-	WMA = values[14:28]
-	EMA = values[28:]
-	return render_template('averages.html', sma=SMA, wma=WMA, ema=EMA)
+	SMA = values[:(last-1)]
+	WMA = values[(last-1):(last*2)]
+	EMA = values[(last*2):]
+	return render_template('averages.html', sma=SMA, wma=WMA, ema=EMA, labels=labels)
 
 @app.route("/bband", methods=['GET', 'POST'])
 def bband():
 	if request.method == 'POST':
 		session['symbol'] = request.form['symbol'].upper()
+		session['period'] = request.form['prediction']
 		return redirect(url_for('bband'), code=307)
 	else:
 		symbol = session.get('symbol', None)
+		period = session.get('period', None)
+		if period == 'short':
+			last = 15
+		else:
+			last = 31
 		technical = ["Real Upper Band", "Real Lower Band", "Real Middle Band"]
+		labels = list(range(1, last))
 		values = []
 		for x in technical:
-			fetch = "https://www.alphavantage.co/query?function=BBANDS&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
+			fetch = "https://www.alphavantage.co/query?function=BBANDS&symbol="+symbol+"&interval=daily&time_period="+str(last-1)+"&series_type=open&apikey="
 			APIkey = "NYZSIAK5PJ3XRW8ASSS"
 			r = requests.get(fetch+APIkey)
 			data = r.json()
 			count = 0
 			for date in data["Technical Analysis: BBANDS"].keys():
 				count = count + 1
-				if count == 15:
+				if count == last:
 					break
 				else:
 					value = float(data["Technical Analysis: BBANDS"][date][x])
 					values.append(value)
-		UBB = values[:14]
-		LBB = values[14:28]
-		MBB = values[28:]
-		return render_template('bband.html', ubb=UBB, mbb=MBB, lbb=LBB)
+		UBB = values[:(last-1)]
+		LBB = values[(last-1):(last*2)]
+		MBB = values[(last*2):]
+		return render_template('bband.html', ubb=UBB, mbb=MBB, lbb=LBB, labels=labels)
 
 @app.route("/adx", methods=['GET', 'POST'])
 def adx():
 	if request.method == 'POST':
 		session['symbol'] = request.form['symbol'].upper()
+		session['period'] = request.form['prediction']
 		return redirect(url_for('adx'), code=307)
 	else:
 		symbol = session.get('symbol', None)
+		period = session.get('period', None)
+		if period == 'short':
+			last = 15
+		else:
+			last = 31
 		values = []
-		fetch = "https://www.alphavantage.co/query?function=ADX&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
+		labels = list(range(1, last))
+		fetch = "https://www.alphavantage.co/query?function=ADX&symbol="+symbol+"&interval=daily&time_period="+str(last-1)+"&series_type=open&apikey="
 		APIkey = "NYZSIAK5PJ3XRW8ASSS"
 		r = requests.get(fetch+APIkey)
 		data = r.json()
 		count = 0
 		for date in data["Technical Analysis: ADX"].keys():
 			count = count + 1
-			if count == 15:
+			if count == last:
 				break
 			else:
 				value = float(data["Technical Analysis: ADX"][date]["ADX"])
 				values.append(value)
 		ADX = values
-		return render_template('adx.html', adx=ADX)
+		return render_template('adx.html', adx=ADX, labels=labels)
 
 if __name__ == '__main__':
 	app.run(debug=True)
