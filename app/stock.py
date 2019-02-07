9f407e718576f947a6e3303204ba9f904ce4338e
InfernapeXavier <itsgood37@ymail.com>
2019-02-07 09:22:07 +0530

    Update stock.py

--- app/stock.py
+++ app/stock.py
@@ -39,48 +39,55 @@ def averages():
 	EMA = values[28:]
 	return render_template('averages.html', sma=SMA, wma=WMA, ema=EMA)
 
-@app.route("/bband")
+@app.route("/bband", methods=['GET', 'POST'])
 def bband():
-	symbol = session.get('symbol', None)
-	technical = ["Real Upper Band", "Real Lower Band", "Real Middle Band"]
-	values = []
-	for x in technical:
-		fetch = "https://www.alphavantage.co/query?function=BBANDS&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
+	if request.method == 'POST':
+		session['symbol'] = request.form['symbol'].upper()
+		return redirect(url_for('bband'), code=307)
+	else:
+		symbol = session.get('symbol', None)
+		technical = ["Real Upper Band", "Real Lower Band", "Real Middle Band"]
+		values = []
+		for x in technical:
+			fetch = "https://www.alphavantage.co/query?function=BBANDS&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
+			APIkey = "NYZSIAK5PJ3XRW8ASSS"
+			r = requests.get(fetch+APIkey)
+			data = r.json()
+			count = 0
+			for date in data["Technical Analysis: BBANDS"].keys():
+				count = count + 1
+				if count == 15:
+					break
+				else:
+					value = float(data["Technical Analysis: BBANDS"][date][x])
+					values.append(value)
+		UBB = values[:14]
+		LBB = values[14:28]
+		MBB = values[28:]
+		return render_template('bband.html', ubb=UBB, mbb=MBB, lbb=LBB)
+
+@app.route("/adx", methods=['GET', 'POST'])
+def adx():
+	if request.method == 'POST':
+		session['symbol'] = request.form['symbol'].upper()
+		return redirect(url_for('adx'), code=307)
+	else:
+		symbol = session.get('symbol', None)
+		values = []
+		fetch = "https://www.alphavantage.co/query?function=ADX&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
 		APIkey = "NYZSIAK5PJ3XRW8ASSS"
 		r = requests.get(fetch+APIkey)
 		data = r.json()
 		count = 0
-		for date in data["Technical Analysis: BBANDS"].keys():
+		for date in data["Technical Analysis: ADX"].keys():
 			count = count + 1
 			if count == 15:
 				break
 			else:
-				value = float(data["Technical Analysis: BBANDS"][date][x])
+				value = float(data["Technical Analysis: ADX"][date]["ADX"])
 				values.append(value)
-	UBB = values[:14]
-	LBB = values[14:28]
-	MBB = values[28:]
-	return render_template('bband.html', ubb=UBB, mbb=MBB, lbb=LBB)
-
-@app.route("/adx")
-def adx():
-	symbol = session.get('symbol', None)
-	values = []
-	fetch = "https://www.alphavantage.co/query?function=ADX&symbol="+symbol+"&interval=daily&time_period=14&series_type=open&apikey="
-	APIkey = "NYZSIAK5PJ3XRW8ASSS"
-	r = requests.get(fetch+APIkey)
-	data = r.json()
-	count = 0
-	for date in data["Technical Analysis: ADX"].keys():
-		count = count + 1
-		if count == 15:
-			break
-		else:
-			value = float(data["Technical Analysis: ADX"][date]["ADX"])
-			values.append(value)
-	ADX = values
-	# print (ADX)
-	return render_template('adx.html', adx=ADX)
+		ADX = values
+		return render_template('adx.html', adx=ADX)
 
 if __name__ == '__main__':
 	app.run(debug=True)
